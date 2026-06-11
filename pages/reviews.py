9776262f86a2731ca32e2cd6060d569efce00e71
BUDESCODE <admin@Admins-MacBook-Pro.local>
2026-06-11 20:10:22 +0100

    sellers and reviews page designed

--- pages/reviews.py
+++ pages/reviews.py
@@ -1,10 +1,143 @@
-from dash import register_page 
+from dash import register_page, html, dcc
 import dash_bootstrap_components as dbc
-
+from config.data_loader import DataLoader, DataCategory 
+import plotly.express as px
+import pandas as pd
+import dash_ag_grid as dag
 register_page(__name__, path="/reviews", name="Reviews")
+
+
+reviews_df = DataLoader(data_category=DataCategory.ORDER_REVIEWS).load_data()
+reviews_df["review_creation_date"] = pd.to_datetime(reviews_df["review_creation_date"])
+
+monthly_scores = reviews_df.set_index("review_creation_date").resample("ME")["review_score"].mean().reset_index()
+
+
+one_star =  len(reviews_df[ reviews_df["review_score"] == 1 ])
+one_star_fmt = f"{one_star:,}"
+two_star = len(reviews_df[ reviews_df["review_score"] == 2 ])
+two_star_fmt = f"{two_star:,}"
+three_star = len(reviews_df[ reviews_df["review_score"] == 3 ])
+three_star_fmt = f"{three_star:,}"
+four_star = len(reviews_df[ reviews_df["review_score"] == 4 ])
+four_star_fmt = f"{four_star:,}"
+five_star = len(reviews_df[ reviews_df["review_score"] == 5 ])
+five_star_fmt = f"{five_star:,}"
+all_stars = one_star + two_star + three_star + four_star + five_star
+all_stars_fmt = f"{all_stars:,}"
+
+score_counts = reviews_df["review_score"].value_counts().reset_index()
+
+
+
+
+def stars_cards(title, star_number):
+    return dbc.Col(
+        md=2,
+        children=[
+            dbc.Card(
+                dbc.CardBody(
+                    html.Div(
+                        children=[
+                            html.Div(
+                                children=[
+                                    html.H6(title, className="text-muted fs-6"),
+                                    html.H4(star_number, className="text-dark")
+                                ]
+                            ),
+                            html.I(className="bi bi-star-fill fs-4 text-primary")
+                        ],
+                        className="d-flex justify-content-between align-items-center"
+                    )
+                )
+            )
+        ]
+    )
+
 
 layout = dbc.Container(
     children=[
-        "Reviews Page"
+        dbc.Row(
+            children=[
+                stars_cards("All Stars", all_stars_fmt),
+                stars_cards("One Star", one_star_fmt),
+                stars_cards("Two Stars", two_star_fmt),
+                stars_cards("Three Stars", three_star_fmt),
+                stars_cards("Four Stars", four_star_fmt),
+                stars_cards("Five Stars", five_star_fmt),
+
+            ],
+            className="mt-3 mb-3"
+        ),
+        dbc.Row(
+            dbc.Col(
+                md=12,
+                children=[
+                    dbc.Card(
+                        dbc.CardBody(
+                            dcc.Graph(
+                                figure = px.bar(
+                                    score_counts,
+                                    x="review_score",
+                                    y="count",
+                                    title="Reviews by Score",
+                                    color="review_score",
+                                    labels={"count":"Total Count", "review_score":"Score"}
+                                )   
+                            )
+                        )
+                    )
+                ]
+            ),
+            className="mb-3"
+        ),
+
+        dbc.Row(
+            children=[
+                dbc.Col(
+                    md=12,
+                    children = [
+                        dbc.Card(
+                            dbc.CardBody(
+                                dcc.Graph(
+                                    figure = px.line(
+                                        monthly_scores,
+                                        x="review_creation_date",
+                                        y="review_score",
+                                        markers=True,
+                                        labels={"review_score":"Average Score", "review_creation_date":"Date"}
+                                    )
+                                )
+                            )
+                        )
+                    ]
+                )   
+            ],
+            className="mb-3"
+        ),
+        dbc.Row(
+            children=[
+                dbc.Col(
+                    md=12,
+                    children = [
+                        dbc.Card(
+                            dbc.CardBody(
+                                children=[
+                                    html.H5("Reviews", className="card-title mb-3"),
+                                    dag.AgGrid(
+                                        columnDefs=[ {"headerName":x.replace("_", " ").title(), "field":x, "sortable":True, "filter":True} for x in reviews_df.columns ],
+                                        rowData=reviews_df.to_dict("records"),
+                                        style={"height":"500px"},
+                                        
+                                    )
+
+                                ]
+                            )
+                        )
+                    ]
+                )
+            ]
+        )
+
     ]
 )
--- pages/sellers.py
+++ pages/sellers.py
@@ -1,10 +1,242 @@
-from dash import register_page 
+from dash import register_page, dcc, html, callback, Output, Input 
 import dash_bootstrap_components as dbc
+import plotly.express as px
+from config.data_loader import DataCategory, DataLoader
 
 register_page(__name__, path="/sellers", name="Sellers")
 
+sellers_df = DataLoader(data_category=DataCategory.SELLERS).load_data()
+geolocation_df = DataLoader(data_category=DataCategory.GEO_LOCATION).load_data()
+geo_grouped = geolocation_df.groupby("geolocation_zip_code_prefix").first().reset_index()
+sellers_with_geo = sellers_df.merge(
+    geolocation_df,
+    how="left",
+    left_on="seller_zip_code_prefix",
+    right_on="geolocation_zip_code_prefix"
+)
+seller_city = sellers_with_geo["seller_city"].unique()
+seller_state = sellers_with_geo["seller_state"].unique()
+total_seller_city = f"{len(seller_city):,}"
+total_seller_state = f"{len(seller_state):,}"
+total_sellers = f"{len(sellers_with_geo):,}"
+seller_state_count = sellers_with_geo["seller_state"].value_counts().reset_index()
+
 layout = dbc.Container(
     children=[
-        "Sellers Page"
+        #  ==============================================filter row======================================
+       dbc.Row(
+           children=[
+               dbc.Col(
+                   md=4,
+                   children=[
+                       dcc.Dropdown(
+                           multi=True,
+                           id="seller-state",
+                           options=seller_state,
+                           placeholder="Select State",
+                           clearable=True   
+
+                       )
+                   ]
+               ),
+               dbc.Col(
+                   md=4,
+                   children=[
+                       dcc.Dropdown(
+                           multi=True,
+                           id="seller-city",
+                           options=seller_city,
+                           placeholder="Select City",
+                           clearable=True   
+
+                       )
+                   ]
+               ),
+           ],
+           className="mt-3 mb-3"
+       ),
+       #  ==============================================end filter row======================================
+
+        #  ==============================================cards row=========================================
+       dbc.Row(
+           children=[
+               dbc.Col(
+                   md=4,
+                   children=[
+                       dbc.Card(
+                           dbc.CardBody(
+                               [
+                                    html.Div(
+                                        children=[
+                                            html.Div(
+                                                children=[
+                                                    html.H6("Total Sellers", className="text-muted"),
+                                                    html.H3(total_sellers, id="total-sellers", className="text-dark"),
+                                                    html.Span("All time sellers", className="text-success")
+                                                ]
+                                            ),
+                                            html.I(className="bi bi-people-fill fs-1 text-primary")
+                                        ],
+                                        className="d-flex justify-content-between align-items-center"
+                                    )   
+                               ]
+                           )
+                       ),
+                   ]
+               ),
+               dbc.Col(
+                   md=4,
+                   children=[
+                        dbc.Card(
+                           dbc.CardBody(
+                               [
+                                    html.Div(
+                                        children=[
+                                            html.Div(
+                                                children=[
+                                                    html.H6("Total States", className="text-muted"),
+                                                    html.H3(total_seller_state, id="sellers-total-states", className="text-dark"),
+                                                    html.Span("All time states", className="text-success")
+                                                ]
+                                            ),
+                                            html.I(className="bi bi-globe fs-1 text-primary")
+                                        ],
+                                        className="d-flex justify-content-between align-items-center"
+                                    )   
+                               ]
+                           )
+                       ),
+                   ]
+               ),
+               dbc.Col(
+                   md=4,
+                   children=[
+                       dbc.Card(
+                           dbc.CardBody(
+                               [
+                                    html.Div(
+                                        children=[
+                                            html.Div(
+                                                children=[
+                                                    html.H6("Total Cities", className="text-muted"),
+                                                    html.H3(total_seller_city, id="sellers-total-city", className="text-dark"),
+                                                    html.Span("All time cities", className="text-success")
+                                                ]
+                                            ),
+                                            html.I(className="bi bi-globe2 fs-1 text-primary")
+                                        ],
+                                        className="d-flex justify-content-between align-items-center"
+                                    )   
+                               ]
+                           )
+                       ),
+                   ]
+               )
+
+           ]
+       ),
+       #  ==============================================end cards row======================================
+
+       #  ==============================================map row============================================
+
+       dbc.Row(
+           children=[
+               dbc.Col(
+                   md=12,
+                   children=[
+                       dbc.Card(
+                           dbc.CardBody(
+                               children=[
+                                    dcc.Graph(
+                                        id="sellers-map",
+                                        figure = px.scatter_mapbox(
+                                            sellers_with_geo,
+                                            lat="geolocation_lat",
+                                            lon="geolocation_lng",
+                                            hover_name="seller_id",
+                                            zoom=3,
+                                            height=700,
+                                            color="seller_state",
+                                            title = "Sellers distribution across Brazil",
+                                            mapbox_style="open-street-map"
+                                        )
+                                )
+                               ]
+                           )
+                       ),
+
+                   ]
+               )
+           ],
+           className="mb-3 mt-3"
+       ),
+       #  ==============================================end map row============================================
+       dbc.Row(
+           children=[
+               dbc.Card(
+                dbc.CardBody(
+                    children = [
+                        dcc.Graph(
+                            id="sellers-state-graph",
+                            figure=px.bar(
+                                seller_state_count,
+                                x="seller_state",
+                                y="count",
+                                title="Sellers by State",
+                                labels={"count":"Total Count", "seller_state":"State"}
+                            )
+                        )
+                    ]
+                )      
+               ),
+
+           ]
+       )
+
     ]
 )
+
+@callback(
+    Output("total-sellers", "children"),
+    Output("sellers-total-states", "children"),
+    Output("sellers-total-city", "children"),
+    Output("sellers-map", "figure"),
+    Output("sellers-state-graph", "figure"),
+    Input("seller-state", "value"),
+    Input("seller-city", "value")
+
+)
+
+def update_dashboard(seller_state, seller_city):
+    sellers_df_copy = sellers_with_geo.copy()
+
+    if seller_state:
+        sellers_df_copy = sellers_df_copy[ sellers_df_copy["seller_state"].isin(seller_state) ]
+
+    if seller_city:
+        sellers_df_copy = sellers_df_copy[ sellers_df_copy["seller_city"].isin(seller_city) ]
+
+    total_sellers = f"{len(sellers_df_copy):,}"
+    total_seller_state = f"{len(sellers_df_copy['seller_state'].unique()):,}"
+    total_seller_city = f"{len(sellers_df_copy['seller_city'].unique()):,}"
+    seller_state_count = sellers_df_copy["seller_state"].value_counts().reset_index()
+
+    sellers_map_figure = px.scatter_mapbox(
+        sellers_df_copy,
+        lat="geolocation_lat",
+        lon="geolocation_lng",
+        hover_name="seller_id",
+        zoom=3,
+        height=700,
+        color="seller_state",
+        title = "Sellers distribution across Brazil",
+        mapbox_style="open-street-map"
+    )    
+    sellers_bar_figure =px.bar(
+        seller_state_count,
+        x="seller_state",
+        y="count",
+        title="Sellers by State",
+        labels={"count":"Total Count", "seller_state":"State"}
+    )
+    return total_sellers, total_seller_state, total_seller_city, sellers_map_figure, sellers_bar_figure
